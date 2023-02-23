5c09194b7e32d77b964725fd59a503c97336fd9b
Vladislav Ermolin <v_ermolin@flo.health>
2023-02-23 16:43:20 +0200

    - Implement custom rules.

--- detekt-rules/src/main/kotlin/org/example/detekt/CustomRule.kt
+++ detekt-rules/src/main/kotlin/org/example/detekt/CustomRule.kt
@@ -1,10 +1,13 @@
 package org.example.detekt
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtValueArgumentList
 
 class CustomRule(config: Config) : Rule(config) {
 
@@ -14,4 +17,20 @@ class CustomRule(config: Config) : Rule(config) {
         description = "Custom rule example",
         debt = Debt.FIVE_MINS,
     )
+
+    override fun visitValueArgumentList(list: KtValueArgumentList) {
+        super.visitValueArgumentList(list)
+        list.arguments
+            .filter { argument ->
+                argument.stringTemplateExpression != null
+            }
+            .forEach { argument ->
+                val smell = CodeSmell(
+                    issue = issue,
+                    entity = Entity.from(argument),
+                    message = "String templates are forbidden!",
+                )
+                report(smell)
+            }
+    }
 }
--- ktlint-rules/src/main/kotlin/org/example/ktlint/CustomRule.kt
+++ ktlint-rules/src/main/kotlin/org/example/ktlint/CustomRule.kt
@@ -1,6 +1,21 @@
 package org.example.ktlint
 
 import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.ElementType
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 
 class CustomRule : Rule(id = "custom-rule") {
+
+    override fun beforeVisitChildNodes(
+        node: ASTNode,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        super.beforeVisitChildNodes(node, autoCorrect, emit)
+        if (node.elementType == ElementType.VALUE_ARGUMENT) {
+            val child = node.firstChildNode
+            if (child.elementType == ElementType.STRING_TEMPLATE)
+                emit(child.startOffset, "String templates are forbidden!", false)
+        }
+    }
 }
